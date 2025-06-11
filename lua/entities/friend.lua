12aa66ee0a79006ff601bda462285e738c5e991b
HiGuyGaming <56192384+windows10win7posreadywoeskio@users.noreply.github.com>
2025-06-11 17:17:19 +0700

    lovely.mp4

---
+++ lua/entities/friend.lua
@@ -0,0 +1,29 @@
+AddCSLuaFile()
+
+ENT.Base 			= "base_nextbot"
+
+function ENT:Initialize()
+
+	self:SetModel( "models/player/kleiner.mdl" )
+	
+	self.LoseTargetDist	= 2000	-- How far the enemy has to be before we lose them
+	self.SearchRadius 	= 1000	-- How far to search for enemies
+	
+end
+
+
+function ENT:RunBehaviour()
+
+	while ( true ) do							-- Here is the loop, it will run forever
+
+		--self:StartActivity( ACT_WALK )			-- Walk animation
+		self.loco:SetDesiredSpeed( 200 )		-- Walk speed
+		self:MoveToPos( self:GetPos() + Vector( math.Rand( -1, 1 ), math.Rand( -1, 1 ), 0 ) * 300 ) -- Walk to a random place within about 300 units (yielding)
+		--self:StartActivity( ACT_IDLE )			-- Idle animation
+		coroutine.wait(2)						-- Pause for 2 seconds
+
+		coroutine.yield()
+		-- The function is done here, but will start back at the top of the loop and make the bot walk somewhere else
+	end
+
+end
---
+++ lua/weapons/srcfrnd_spawn.lua
@@ -0,0 +1,49 @@
+if SERVER then
+    AddCSLuaFile()
+end
+
+SWEP.PrintName = "frend spawner"
+SWEP.Category = "sourcefriends"
+SWEP.Spawnable = true
+SWEP.AdminOnly = false
+SWEP.SwayScale = 0
+--SWEP.BobScale = 1
+
+SWEP.Primary.ClipSize = -1
+SWEP.Primary.DefaultClip = -1
+SWEP.Primary.Automatic = false
+SWEP.Primary.Ammo = "none"
+
+SWEP.Secondary.ClipSize = -1
+SWEP.Secondary.DefaultClip = -1
+SWEP.Secondary.Automatic = false
+SWEP.Secondary.Ammo = "none"
+
+SWEP.UseHands = false
+SWEP.ViewModel = "models/weapons/v_pistol.mdl"
+SWEP.WorldModel = "models/weapons/w_pistol.mdl"
+
+function SWEP:Initialize()
+    self:SetHoldType("pistol")
+end
+
+function SWEP:PrimaryAttack()
+    if CLIENT then return end
+
+    local owner = self:GetOwner()
+    local tr = owner:GetEyeTrace()
+    local pos = owner:GetShootPos() + owner:GetAimVector() * 64
+
+    local ent = ents.Create("friend")
+    if not IsValid(ent) then return end
+
+    ent:EmitSound("hl1/fvox/bell.wav", 75, 50)
+
+    owner:SetAnimation(PLAYER_ATTACK1)
+    self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
+    ent:SetPos(pos)
+    ent:Spawn()
+    ent:Activate()
+    --ent:SetModel("models/player/kleiner.mdl")
+    ent:SetName("frend")
+end
